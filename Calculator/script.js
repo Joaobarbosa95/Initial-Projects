8a6c759012d28059289234403c7eb114ab72c225
Joaobarbosa95 <bticjoaobarbosa@hotmail.com>
2020-12-21 04:06:19 +0000

    the perfect calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -5,24 +5,46 @@ const container = document.querySelector(".container");
 const screen = document.querySelector(".screen");
 
 let calc = "";
+let lastKey = "";
+const operators = ["+", "-", "/", "*", "."];
+let dots = 0;
+
+const update = function (dataSet) {
+  calc += dataSet;
+  lastKey = dataSet;
+  screen.innerHTML = calc;
+};
 
 container.addEventListener("click", (e) => {
   if (e.target.localName === "button") {
-    const dataSet = e.target.attributes[1].value;
-    try {
-      if (dataSet === "=" && calc !== "") {
-        screen.innerHTML = eval(calc);
-        calc = "";
-      } else if (dataSet === "C") {
-        calc = "";
-        screen.innerHTML = "";
-      } else if (dataSet !== "=" && dataSet !== "C") {
-        calc += dataSet;
-        screen.innerHTML = calc;
-      }
-    } catch (err) {
-      screen.innerHTML = "error";
+    const dataSet = e.target.getAttribute("data-set");
+
+    if (dataSet === "=" && calc !== "") {
+      const equal = eval(calc);
+      screen.innerHTML = equal;
+      calc = equal;
+    } else if (dataSet === "C") {
       calc = "";
+      lastKey = "";
+      dots = 0;
+      screen.innerHTML = "";
+    } else if (operators.includes(dataSet)) {
+      if (calc.length < 1) return;
+      if (dots === 1 && dataSet !== ".") {
+        dots = 0;
+        update(dataSet);
+      } else if (dots === 0 && dataSet === ".") {
+        dots++;
+        update(dataSet);
+      } else if (
+        (dots === 1 && dataSet === ".") ||
+        operators.includes(lastKey)
+      ) {
+      } else {
+        update(dataSet);
+      }
+    } else {
+      update(dataSet);
     }
   }
 });
